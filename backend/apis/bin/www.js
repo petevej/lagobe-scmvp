f28b8852ab22da86a9e14295b4d38d9a8fccf633
Manit Kwanyun <a@muime.com>
2017-04-17 16:36:58 +0700

    Fix bug: effect from using https

--- backend/apis/bin/www.js
+++ backend/apis/bin/www.js
@@ -2,6 +2,8 @@ require.main.require('../global');
 
 var app = require(global.modulePath('app')); //Require our app
 var http = require('http');
+//var https = require('https');
+//var fs = require('fs');
 
 app.set('port', process.env.PORT || 8000);
 
@@ -9,6 +11,11 @@ var server = http.createServer(app).listen(app.get('port'), function() {
     console.log('Express server listening on port ' + server.address().port);
 });
 
-//var server = app.listen(app.get('port'), function() {
+//// Create an HTTPS service identical to the HTTP service.
+//var options = {
+//    key: fs.readFileSync('/etc/letsencrypt/live/sc.lagobe.com/privkey.pem'),
+//    cert: fs.readFileSync('/etc/letsencrypt/live/sc.lagobe.com/fullchain.pem')
+//};
+//var server = https.createServer(options, app).listen(app.get('port'), function () {
 //    console.log('Express server listening on port ' + server.address().port);
 //});
